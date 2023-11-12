97fb6492c26f5fe6a6aff3c85528ca78a7c7ad56
Aditya Bhattad <adityabhattad18@gmail.com>
2023-11-12 23:57:17 +0530

    add chat functionality

--- pdf-dist/app/chat/create_embeddings.py
+++ pdf-dist/app/chat/create_embeddings.py
@@ -1,6 +1,6 @@
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from app.chat.embeddings.vector_stores.pipecone import vector_store
+from app.chat.vector_stores.pipecone import vector_store
 
 def create_embeddings_for_pdf(pdf_id: str, pdf_path: str):
     text_splitter = RecursiveCharacterTextSplitter(
---
+++ pdf-dist/app/chat/llms/chatopenai.py
@@ -0,0 +1,6 @@
+from langchain.chat_models import ChatOpenAI
+
+
+
+def build_llm(chat_args):
+    return ChatOpenAI()
