12f363a7eb9ec093d68d4086f2fdd811ab967f57
Aditya Bhattad <adityabhattad18@gmail.com>
2023-11-14 14:27:47 +0530

    add streaming chat option

--- pdf-dist/app/chat/chat.py
+++ pdf-dist/app/chat/chat.py
@@ -1,8 +1,9 @@
-from langchain.chains import ConversationalRetrievalChain
+from app.chat.chains.retrieval import StreamingConversationalRetirevalChain
 from app.chat.models import ChatArgs
 from app.chat.vector_stores.pipecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
+from langchain.chat_models import ChatOpenAI
 
 
 
@@ -11,8 +12,9 @@ def build_chat(chat_args: ChatArgs):
     llm = build_llm(chat_args=chat_args)
     memory = build_memory(chat_args=chat_args)
 
-    return ConversationalRetrievalChain.from_llm(
+    return StreamingConversationalRetirevalChain.from_llm(
         llm=llm,
         memory=memory,
-        retriever=retriever
+        retriever=retriever,
+        condense_question_llm=ChatOpenAI(streaming=False),
     )
